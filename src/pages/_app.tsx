9817a503f0cf513b7f36bb8899d90937356aa4b8
Rafaele <rafaele.sinaguglia@gmail.com>
2023-08-14 20:25:36 +0200

    clerk

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { type AppType } from "next/app";
+import { api } from "~/utils/api";
+import "~/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <ClerkProvider>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
